1a88085ef78d86f5bba1fa1dc0c24ccfb3e0a79a
ARYANKUMAR0307 <104307893+ARYANKUMAR0307@users.noreply.github.com>
2022-06-18 23:52:32 +0530

    Create Assignment 5_outputs.py

---
+++ Assignment 5_outputs.py
@@ -0,0 +1,130 @@
+#Output_Question_1
+
+Enter the string to be reversedAHALAWT
+TWALAHA
+
+
+#Output_Question_2
+
+
+Enter starting of Range
+5
+Enter end of Range
+9
+Enter the number
+4
+Numbers in range  5  to  9  divisible by  4  are:
+8
+
+
+#Output_Question_3
+
+
+Enter side A
+5
+Enter side B
+6
+Enter side C
+7
+Area of the triangle is:  14.696938456699069
+
+  
+#Output_Question_4
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+ 
+  
+#Output_Question_5
+
+
+Enter the no of rows: 7
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+
+
+#Output_Question_6
+
+
+Please, Enter the Lowest Range Value : 
+5
+Please, Enter the Upper Range Value : 
+9
+
+The Prime Numbers in the range are: 
+1 ) 5
+2 ) 7
+
+
+#Output_Question_7
+
+
+77
+154
+231
+308
+385
+462
+
+
+#Output_Question_8
+
+
+Enter 1th number: 10
+Enter 2th number: 20
+Enter 3th number: 30
+Enter 4th number: 40
+Enter 5th number: 50
+Enter 6th number: 60
+Enter 7th number: 70
+Enter 8th number: 80
+Enter 9th number: 90
+Enter 10th number: 100
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Positive numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+Negative numbers are: 
+Odd numbers are: 
+Even numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+No of times each number occurs in the List:
+{10: 1, 20: 1, 30: 1, 40: 1, 50: 1, 60: 1, 70: 1, 80: 1, 90: 1, 100: 1}
+
+
+#Output_Question_9
+
+
+Enter a string: ahalawat
+{'ahalawat': 1}
+  
