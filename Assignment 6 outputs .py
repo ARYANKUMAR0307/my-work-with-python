cdd9cf9ed356a02c31a9e7114a74b315975020c2
ARYANKUMAR0307 <104307893+ARYANKUMAR0307@users.noreply.github.com>
2022-06-27 12:47:56 +0530

    Create Assignment 6 outputs .py

---
+++ Assignment 6 outputs .py
@@ -0,0 +1,35 @@
+#Output for question 1
+
+Enter a number: 56
+Entered number is not a perfect number
+
+Enter a number: 28
+Entered number is a perfect number
+
+
+#output for question 2
+
+Enter a string: madam
+String is palindrome
+
+Enter a string: Nurses run
+String is palindrome
+
+Enter a string: aryan
+String is not palindrome
+
+
+#output for question 3
+
+Enter a nunmber: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+  
+  
+#output for question 4
+
+
+  
