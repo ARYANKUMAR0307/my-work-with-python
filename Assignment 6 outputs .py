d2e5d21efa0baa51f2d703bb05da101669eded11
ARYANKUMAR0307 <104307893+ARYANKUMAR0307@users.noreply.github.com>
2022-07-03 23:13:00 +0530

    Update Assignment 6 outputs .py

--- Assignment 6 outputs .py
+++ Assignment 6 outputs .py
@@ -1,35 +1,94 @@
 #Output for question 1
 
 Enter a number: 56
-Entered number is not a perfect number
+56  is not a perfect number
 
 Enter a number: 28
-Entered number is a perfect number
+28  is a perfect number
 
 
-#output for question 2
+#Output for question 2
 
-Enter a string: madam
-String is palindrome
+Enter a String: madam
+madam is a palindrome
 
-Enter a string: Nurses run
-String is palindrome
-
-Enter a string: aryan
-String is not palindrome
+Enter a String: aryan
+aryan is not a palindrome
 
 
-#output for question 3
+#Output for question 3
 
-Enter a nunmber: 5
-      1 
-     1 1 
-    1 2 1 
-   1 3 3 1 
-  1 4 6 4 1 
-  
-  
-#output for question 4
+Enter a number: 7
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+      
 
+#Output for question 4
+
+Enter a String: I love my country
+No 'I love my country' is not a panagram
+
+Enter a String:  Pack my box with five dozen liquor jugs
+Yes ' Pack my box with five dozen liquor jugs' is a panagram
+
+
+#Output for question 5
+
+Enter a hyphen separated sentence: I-love-to-play-badminton
+I-badminton-love-play-to
+
+
+#Output for question 6
+
+Student ID= 21102097
+Student ID= 21102097
+Student Name= Aryan Kumar
+Student ID= 21102097
+Student Name= Aryan Kumar
+Student Class= 1st Year
+
+
+#Output for question 7
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+
+#Output for question 8
+
+Enter number of elements : 8
+-25
+-10
+-7
+-3
+2
+4
+8
+10
+-10 2 8
+-7 -3 10
+None
+
+
+#Output for question 9
+
+Enter the sequence of parantheses : {}[]()
+{}[]() - is balanced
+
+Enter the sequence of parantheses : ()[]{}
+()[]{} - is balanced
+
+#Output for question 10
 
   
